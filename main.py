a85ef336ee18aa5efdddfdcd0aede10d84167525
quan <txtgroup>
2022-05-04 10:07:49 +0200

    Work well on debug running time

--- main.py
+++ main.py
@@ -10,15 +10,30 @@ if __name__ == '__main__':
         wb = xl.Workbooks.Open(os.path.abspath(sys.argv[1]))
         excel_name = os.path.basename(sys.argv[1])
         try:
+            # Note: Macro_1 should not be run since it was fixed to file path of Andrea
+            #       real macro from 2 to 7
+            # xl.Application.Run("m4.xlsm!Module1.Macro_1")
+
+            xl.Application.Run("m4.xlsm!Module1.Macro_2")
             xl.Application.Run("m4.xlsm!Module1.Macro_3")
+            xl.Application.Run("m4.xlsm!Module1.Macro_4")
+            xl.Application.Run("m4.xlsm!Module1.Macro_5")
+            xl.Application.Run("m4.xlsm!Module1.Macro_6")
+            xl.Application.Run("m4.xlsm!Module1.Macro_7")
+
             # NOT WORK BELOW!!!!
-            # xl.Application.Run("D:\\excle_macro_temp\\m7.xlsm!Module1.Macro_3")
+            # xl.Application.Run("D:\\excle_macro_temp\\m4.xlsm!Module1.Macro_2")
+            # xl.Application.Run("D:\\excle_macro_temp\\m4.xlsm!Module1.Macro_3")
+            # xl.Application.Run("D:\\excle_macro_temp\\m4.xlsm!Module1.Macro_4")
+            # xl.Application.Run("D:\\excle_macro_temp\\m4.xlsm!Module1.Macro_5")
+            # xl.Application.Run("D:\\excle_macro_temp\\m4.xlsm!Module1.Macro_6")
+            # xl.Application.Run("D:\\excle_macro_temp\\m4.xlsm!Module1.Macro_7")
         except:
             xl.Application.Quit()  # Comment this out if your excel script closes
             del xl
             exit
 
-        # wb.SaveAs(Filename="D:\\excle_macro_temp\\" + "macro_" + excel_name)
+        wb.SaveAs(Filename="D:\\excle_macro_temp\\" + "macro_" + excel_name)
         # wb.SaveAs(Filename="D:\\excle_macro_temp\\ciao.xlsx")
 
         wb.Close()
